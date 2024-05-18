c8f79ce24c17b96140a508742bf191488f04fa8d
Franklin Mvoi <mvoifranklin@gmail.com>
2024-05-19 01:51:19 +0300

    cadavers... Damn

--- models/base_model.py
+++ models/base_model.py
@@ -25,12 +25,13 @@ Write a class BaseModel that defines all common attributes/methods for other cla
 import json
 from uuid import uuid4
 from datetime import date, datetime
+import time
 
 class BaseModel:
     """Super class fro all"""
     def __init__(self) -> None:
         self.id = str(uuid4())
-        self.created_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+        self.created_at = datetime.now()#.strftime("%Y-%m-%dT%H:%M:%S")
         self.updated_at = self.created_at
         
     def __str__(self) -> str:
@@ -38,13 +39,29 @@ class BaseModel:
 
     def save(self):
         """sets updated_at to current datetime"""
-        self.updated_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+        self.updated_at = datetime.now()#.strftime("%Y-%m-%dT%H:%M:%S")
         
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__ of the instance"""
         master_dict = self.__dict__
         
-        master_dict["created_at"] = master_dict["created_at"].isoformat()
-        master_dict["updated_at"] = master_dict["updated_at"].isoformat()
+        master_dict["created_at"] = master_dict["created_at"].isoformat() # self.created_at.isoformat()
+        master_dict["updated_at"] = master_dict["updated_at"].isoformat() # self.updated_at.isoformat()
         master_dict["__class__"] = self.__class__.__name__
         return master_dict
+
+model = BaseModel()
+print(model)
+time.sleep(10)
+model.save()
+print("\n")
+print(model)
+print(type(model.created_at))
+print(type(model.updated_at))
+
+json_repr = model.to_dict()
+
+print("\n")
+print(json_repr)
+print(type(json_repr["created_at"]))
+print(type(json_repr["updated_at"]))
